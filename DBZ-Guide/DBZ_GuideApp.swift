e3994e15e72f8aa5ac6a16e4543b50a6fa5c7225
Ethan Borrowman <ethan_borrowman@hotmail.com>
2024-07-08 12:09:59 -0500

    Initial Commit

---
+++ DBZ-Guide/DBZ_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBZ_GuideApp.swift
+//  DBZ-Guide
+//
+//  Created by Ethan Borrowman on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DBZ_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DBZ-GuideTests/DBZ_GuideTests.swift
@@ -0,0 +1,16 @@
+//
+//  DBZ_GuideTests.swift
+//  DBZ-GuideTests
+//
+//  Created by Ethan Borrowman on 7/8/24.
+//
+
+import Testing
+
+struct DBZ_GuideTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
