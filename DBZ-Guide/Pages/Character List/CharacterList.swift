b893a8ae87fec86d94b5db0fcbdb9b2263962ba7
Ethan Borrowman <ethan_borrowman@hotmail.com>
2024-08-01 13:10:58 -0500

    Add character list, not sure what was going on with this

---
+++ DBZ-Guide/Pages/Character List/CharacterList.swift
@@ -0,0 +1,39 @@
+//
+//  CharacterList.swift
+//  DBZ-Guide
+//
+//  Created by Ethan Borrowman on 7/9/24.
+//
+
+import SwiftUI
+
+struct CharacterList: View {
+    
+    @State var viewModel: CharacterListViewModel
+    
+    var body: some View {
+        switch viewModel.state {
+        case .loaded: characterGrid()
+        default: Text("Not Implemented")
+        }
+    }
+    
+    @ViewBuilder private func characterGrid() -> some View {
+        let columns = DeviceType.shared.isIpad ?
+        [GridItem(.flexible()), GridItem(.flexible())] :
+        [GridItem(.flexible())]
+        
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(Array(viewModel.characters.enumerated()), id: \.element.id) { index, character in
+                    CharacterCard(viewModel: CharacterCardViewModel(character: character, displayIndex: index))
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    CharacterList(viewModel: CharacterListViewModel())
+}
